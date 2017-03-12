6cc474e3c2f31760ea8199e97caf1053e226c08a
Santiago Rincón <rincorpes@gmail.com>
2017-03-12 16:51:13 -0400

    HTML5 index

--- index.php
+++ index.php
@@ -4,5 +4,37 @@
  *
  * @package vbt
  */
-echo __('Hola Mundo!');
 ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="es" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="es" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="es" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="es" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="es" class="no-js">
+<!--<![endif]-->
+
+<head>
+	<!-- Google Adsense code here -->
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<header>
+		Header
+	</header>
+	<div role="main">
+		Main content
+	</div>
+	<footer>
+		Footer
+	</footer>
+	<?php wp_footer(); ?>
+</body>
+
+</html>
