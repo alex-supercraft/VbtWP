e8177dc60a5f91f352a8ab5f78ea16717681ac3f
Santiago Rincón <rincorpes@gmail.com>
2017-03-13 12:30:07 -0400

    Comments styles

--- comments.php
+++ comments.php
@@ -35,14 +35,24 @@
 	/**
 	 * Personalizar formulario
 	 */
-	$fields =  array(
+	/*$fields =  array(
 		'author' => '<p class="comment-form-author"><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" placeholder="' . __( 'Nombre' ) . '" '. ( $req ? 'required' : '' ) . ' /></p>',
 		'email'  => '<p class="comment-form-email"><input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" placeholder="' . __( 'Correo' ) . '" '. ( $req ? 'required' : '' ) . ' /></p>',
 		'url'    => '<p class="comment-form-url"><input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" placeholder="' . __( 'Sitio Web' ) . '" /></p>',
-	); 
-	$comment_field = '<p class="comment-form-comment"><textarea id="comment" name="comment" cols="45" rows="8" placeholder="'. _x( 'Comment', 'noun' ) . '" required></textarea></p>';
+	); */
+	$comment_field = '<textarea id="comment" class="form-control"  name="comment" cols="45" rows="8" placeholder="'. __( 'Escribe tu opinión', 'vbt' ) . '" required></textarea>';
 	?>
-	
-	<?php //comment_form(array('fields' => $fields , 'comment_field' => $comment_field)); ?>
-	<?php comment_form(); ?>
+	<div class="comment-form-wrapp">
+		<?php
+		comment_form(
+			array(
+				'title_reply' => __('¿Y tú qué opinas?', 'vbt'), // Change title
+				'submit_button' => '<input name="%1$s" type="submit" id="%2$s" class="%3$s btn btn-default" value="%4$s" >', // Change button
+				'comment_field' => $comment_field, // Change textarea
+				'comment_notes_before' => '', // Delete notes before comment form
+				'comment_notes_after' => '<span class="help-block">' . __( 'Por si acaso, tu email no se mostrará.', 'vbt' ) . '</span>' // Edit notes after form
+				)
+			);
+		?>
+	</div>
 </section>
