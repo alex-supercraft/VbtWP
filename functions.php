88b568c3507f113624ddafd43dbd89c643fbef29
Santiago Rincón <rincorpes@gmail.com>
2017-03-12 20:12:34 -0400

    Added logo and footer

--- functions.php
+++ functions.php
@@ -46,8 +46,8 @@ if (! function_exists('vbt_setup')):
 
 		// Enable suppor for custom Logo
 		add_theme_support('custom-logo', array(
-			'height' => 100,
-			'width' => 400,
+			'height' => 50,
+			'width' => 300,
 			'flex-height' => true,
 			'flex-width' => true,
 			'header-text' => array('site-title', 'site-description')
--- index.php
+++ index.php
@@ -24,6 +24,8 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+	<!-- #vbt-header -->
 	<header id="vbt-header">
 		<!-- Navbar Fixed Top -->
 		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -37,11 +39,29 @@
 						data-target=".navbar-ex1-collapse">
 						<span class="glyphicon glyphicon-menu-hamburger"></span>
 					</button>
+					<div class="site-logo visible-xs">
+						<?php if (! the_custom_logo()): ?>
+							<a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
+								<img
+									alt="vbt logo"
+									src="<?php echo get_template_directory_uri(); ?>/images/vbt-logo.png">
+							</a>
+						<?php endif ?>
+					</div>
 				</div>
 				<!-- Navbar Content -->
 				<div class="collapse navbar-collapse navbar-ex1-collapse">
-					<div class="site-identity navbar-left">
-						<h1>
+					<div class="site-identity navbar-left hidden-xs">
+						<h1 class="text-hide">
+							<?php if (! the_custom_logo()): ?>
+								<a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
+									<img
+										alt="vbt logo"
+										src="<?php echo get_template_directory_uri(); ?>/images/vbt-logotipo.png">
+								</a>
+							<?php else: ?>
+								<?php the_custom_logo(); ?>
+							<?php endif; ?>
 							<?php bloginfo('name'); ?>
 						</h1>
 					</div> 
@@ -58,12 +78,27 @@
 			</div>
 		</nav>
 	</header>
+	<!--  end #vbt-header -->
+
+	<!-- #vbt-main -->
 	<div role="main" id="vbt-main">
 		Main content
 	</div>
+	<!-- end #vbt-main -->
+
+	<!-- #vbt-footer -->
 	<footer id="vbt-footer">
-		Footer
+		<div class="footer-info">
+			<div class="container">
+				<p>
+					<?php echo __('Plantilla diseñada por'); ?> <a href="https://twitter.com/rincorpes"><?php echo __('Rincorpes'); ?></a> <?php echo __('con tecnología'); ?> <a href="http://wordpress.org"><?php echo __('Wordpress.org'); ?></a><br />
+					© <a href="http://www.github.com/rincorpes/vbt-wp-theme">Vbt Wp Theme</a> 2017
+				</p>
+			</div>
+		</div>
+		</div>
 	</footer>
+	<!-- end #vbt-footer -->
 
 	<!-- IE bootstrap support -->
 
